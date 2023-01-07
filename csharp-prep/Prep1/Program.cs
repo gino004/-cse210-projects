269a20d84d2c92597a6ed00fed3f2a6fc48c4aa8
gino004 <ang22005@byui.edu>
2023-01-07 15:42:39 -0500

    C# basico

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // conlose write sirve para hacer una entrada de dato
+        Console.Write("What is your first name? ");
+        // string first es para hacer uso final de la console write en la salida y al igual que abajo es el mismo proceso
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last},{first} {last}.");
     }
 }
